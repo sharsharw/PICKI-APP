658df76c87c8f050bb3d39bdf6e2253d62e2a493
Sharleen Wang <sharleenwang7@gmail.com>
2025-05-20 22:55:57 -0700

    Update next.config.mjs and package.json

--- picki/next.config.mjs
+++ picki/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
-  basePath: '/PICKI-APP/picki', 
+  output: 'export',  
+  basePath: '/PICKI-APP/picki',  
 };
+
 export default nextConfig;
